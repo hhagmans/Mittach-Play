d57f3db0120a91df077b0747aa65d67990240f44
Christian Oberschulte <co@innoq.com>
2012-10-11 17:50:02 +0200

    new fixtures.yml

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,10 +1,18 @@
 import org.junit.*;
+import org.junit.Before;
+
 import play.test.*;
 import play.mvc.*;
 import play.mvc.Http.*;
 import models.*;
 
 public class ApplicationTest extends FunctionalTest {
+	
+	@Before
+	public void setUp() {
+	    Fixtures.deleteDatabase();
+	    Fixtures.loadModels("fixtures.yml");
+	}
 
     @Test
     public void testThatIndexPageWorks() {
@@ -12,6 +20,12 @@ public class ApplicationTest extends FunctionalTest {
         assertIsOk(response);
         assertContentType("text/html", response);
         assertCharset(play.Play.defaultWebEncoding, response);
+        
+        assertContentMatch("Pommes", response);
+        assertContentMatch("Schnitzel", response);
+        assertContentMatch("Spaghetti", response);
+        
+        //assertContentMatch("Auflauf", response);
     }
     
 }
