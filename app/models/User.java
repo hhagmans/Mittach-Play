3390510026b8ed251e12f379bd38f0b52b29b32f
Christian Oberschulte <co@innoq.com>
2012-10-18 14:10:28 +0200

    add bookings to fixtures

--- app/models/User.java
+++ app/models/User.java
@@ -2,24 +2,22 @@ package models;
 
 import java.util.*;
 import javax.persistence.*;
- 
+
 import play.db.jpa.*;
- 
+
 @Entity
 public class User extends Model {
- 
-	
-    public String shortname;
-    public boolean isAdmin;
-    
+
+	public String shortname;
+	public boolean isAdmin;
 
 	public User(String shortname, boolean isAdmin) {
 		this.shortname = shortname;
 		this.isAdmin = isAdmin;
 	}
 
-public static User connect(String shortname) {
-	return find("byShortname", shortname).first();
+	public static User connect(String shortname) {
+		return find("byShortname", shortname).first();
 	}
- 
+
 }
