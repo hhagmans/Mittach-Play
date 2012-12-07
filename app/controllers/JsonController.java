8b296ed24bf8b21b7e90b30c57c27604b796d991
hhagmans <hendrikh@innoq.com>
2012-12-07 15:30:17 +0100

    Events can now be created or updated via JSON, bookings can be created

--- app/controllers/JsonController.java
+++ app/controllers/JsonController.java
@@ -1,14 +1,17 @@
 package controllers;
 
 import java.sql.Date;
+import java.util.ListIterator;
 
 import play.*;
-import play.Logger;
+import play.data.validation.Valid;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
+import models.Booking;
 import models.Event;
+import models.User;
 
 public class JsonController extends BaseController {
 	
@@ -22,7 +25,7 @@ public class JsonController extends BaseController {
 		
 	}
 	
-	public static void handleJsonBody(String body) {
+	public static void createJsonEvent(String body) {
 		// curl -v -H "Content-Type: application/json" -X POST -d '{"title":"dfh","date":"Nov 30, 2012 12:00:00 AM","vegetarian_opt":false,"slots":-1,"details":"dhf","bookings":[],"id":1}' http://localhost:9000/event/json/create
 		Logger.info("content type: %s", request.contentType);
 	    Logger.info("json string: %s", body);
@@ -31,6 +34,43 @@ public class JsonController extends BaseController {
 	    event = new Event(event);
 	    event.save();
 	    AdminController.index(1);
+	}
+	
+	public static void updateJsonEvent(String body) {
+		// curl -v -H "Content-Type: application/json" -X POST -d '{"title":"dfhupdated","date":"Nov 29, 2012 12:00:00 AM","vegetarian_opt":false,"slots":-1,"details":"dhf","bookings":[],"id":1}' http://localhost:9000/event/json/update
+		Logger.info("content type: %s", request.contentType);
+	    Logger.info("json string: %s", body);
+		Event newEvent = new Gson().fromJson(body, Event.class);
+	    Event oldEvent = Event.findById(newEvent.id);
+	    if (oldEvent != null) {
+	    	oldEvent.title = newEvent.title;
+	    	oldEvent.slots = newEvent.slots;
+	    	oldEvent.details = newEvent.details;
+	    	oldEvent.date = newEvent.date;
+	    	oldEvent.vegetarian_opt = newEvent.vegetarian_opt;
+	    	oldEvent.save();
+	    }
+	    AdminController.index(1);
+	}
+	
+	public static void addJsonBooking(String body) {
+		// curl -v -H "Content-Type: application/json" -X POST -d '{"user":{"shortname":"we","isAdmin":false,"id":6},"event":{"title":"dfhupdated","date":"Nov 29, 2012 12:00:00 AM","vegetarian_opt":false,"slots":-1,"details":"dhf","bookings":[],"id":4},"id":6}' http://localhost:9000/event/json/addBooking
+		Booking booking = new Gson().fromJson(body, Booking.class);
+		User user = User.find("byShortname", booking.user.shortname).first();
+		if (user == null){
+			user = new User(booking.user.shortname,false);
+			user.save();
+		}
+		Event event = Event.findById(booking.event.id);
+		if (event != null){
+		if (event.getUsers().contains(user.shortname) == false && user.shortname != ""){
+		Booking newbooking = new Booking(event, user, false);
+		newbooking.save();
+		event.bookings.add(booking);
+		event.save();
+		}
+		}
+		AdminController.index(1);
 	}
 	
 }
